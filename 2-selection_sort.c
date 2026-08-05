e57060bb55fea53c4d377078732b4356af30917f
GitGolik <goliktheo.pro@gmail.com>
2026-08-05 11:19:07 +0200

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,10 +11,9 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, min_index;
 	int tmp;
 
-	if ((!array) || (size < 2))
-	{
+	if (!array || size < 2)
 		return;
-	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
@@ -22,18 +21,16 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
-			{
 				min_index = j;
-			}
+		}
 
-			if (min_index != 1)
-			{
-				tmp = array[i];
-				array[i] = array[min_index];
-				array[min_index] = tmp;
+		if (min_index != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = tmp;
 
-				print_array(array, size);
-			}
+			print_array(array, size);
 		}
 	}
 }
