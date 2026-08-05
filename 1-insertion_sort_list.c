03f989453fe219776fa20f52607d1c5814aec1fa
GitGolik <goliktheo.pro@gmail.com>
2026-08-05 10:59:45 +0200

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,42 +2,38 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *prev, *next;
+	listint_t *current, *prev, *next;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current)
-    {
-        next = current->next;
+	while (current)
+	{
+		next = current->next;
 
-        while (current->prev && current->n < current->prev->n)
-        {
-            prev = current->prev;
+		while (current->prev && current->n < current->prev->n)
+		{
+			prev = current->prev;
 
-            /* relier prev->prev et current */
-            if (prev->prev)
-                prev->prev->next = current;
-            current->prev = prev->prev;
+			if (prev->prev)
+				prev->prev->next = current;
+			current->prev = prev->prev;
 
-            /* relier current et prev->next */
-            if (current->next)
-                current->next->prev = prev;
-            prev->next = current->next;
+			if (current->next)
+				current->next->prev = prev;
+			prev->next = current->next;
 
-            /* relier current et prev */
-            current->next = prev;
-            prev->prev = current;
+			current->next = prev;
+			prev->prev = current;
 
-            /* mettre à jour la tête si nécessaire */
-            if (!current->prev)
-                *list = current;
+			if (!current->prev)
+				*list = current;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        current = next;
-    }
+		current = next;
+	}
 }
